d58bfb0db2e3cc3f7cafd889bb45d3e584246b49
Burning <974201360@qq.com>
2019-03-13 15:42:26 +0800

    创建项目

---
+++ AliyunOSSUploadImage/AliyunOSSUploadImage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AliyunOSSUploadImage
+//
+//  Created by git burning on 2019/3/13.
+//  Copyright © 2019年 git burning. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
